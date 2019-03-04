f2eb6e3ac55b15be7d7f9c912ab49ad5cdfc6b87
DJones2314 <djvjones23@gmail.com>
2019-03-04 16:06:28 +0000

    first commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb://yellowstone:EWssYRcsN8Nz6K9cMTNrBu0xMmLiq1MuZOvKfmf696ArodtGudbi3UFa6vSPVVgjDxAZCA1upmx6ndIQ9lP31A%3D%3D@yellowstone.documents.azure.com:10255/?ssl=true',
+  secretOrKey: "secret"
+};
